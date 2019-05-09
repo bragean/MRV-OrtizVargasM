369506efe56b68ba35195be3ffac9e8ab25e8a6d
JesusOCH <44234311+JesusOCH@users.noreply.github.com>
2019-05-09 00:34:25 -0500

    Capitulo 5 - World Space UI

---
+++ VR_is_awesome/Assets/Scripts/FlippinDashboard.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+public class FlippinDashboard : MonoBehaviour {
+	private HeadGesture gesture;
+	private GameObject dashboard;
+	private bool isOpen = true;
+	private Vector3 startRotation;
+	void Start () {
+		gesture = GetComponent<HeadGesture> ();
+		dashboard = GameObject.Find ("Dashboard");
+		startRotation = dashboard.transform.eulerAngles;
+		CloseDashboard ();
+	}
+	void Update () {
+		if (gesture.isFacingDown) {
+			OpenDashboard ();
+		} else {
+			CloseDashboard ();
+		}
+	}
+	private void CloseDashboard() {
+		if (isOpen) {
+			dashboard.transform.eulerAngles = new Vector3 (180.0f,
+			                                               startRotation.y, startRotation.z);
+			isOpen = false;
+		}
+	}
+	private void OpenDashboard() {
+		if (!isOpen) {
+			dashboard.transform.eulerAngles = startRotation;
+			isOpen = true;
+		}
+	}
+}
---
+++ VR_is_awesome/Assets/Scripts/HeadGesture.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+public class HeadGesture : MonoBehaviour {
+	public bool isFacingDown = false;
+	void Update () {
+		isFacingDown = DetectFacingDown ();
+	}
+	private bool DetectFacingDown () {
+		return (CameraAngleFromGround () < 60.0f);
+	}
+	private float CameraAngleFromGround () {
+		return Vector3.Angle (Vector3.down, Camera.main.transform.rotation
+		                      * Vector3.forward);
+	}
+}
