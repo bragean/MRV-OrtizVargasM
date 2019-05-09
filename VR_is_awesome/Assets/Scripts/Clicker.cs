05812f3f8e869768bf58c2fe714bd518bc65745b
JesusOCH <44234311+JesusOCH@users.noreply.github.com>
2019-05-09 00:26:20 -0500

    Capitulo 3- VR Build and Run

--- VR_is_awesome/Assets/Scripts/Clicker.cs
+++ VR_is_awesome/Assets/Scripts/Clicker.cs
@@ -1,30 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class Clicker{
+	public bool clicked(){
+#if (UNITY_ANDROID || UNITY_IPHONE)
+		return Cardboard.SDK.CardboardTriggered;
+#else
+		return Input.anyKeyDown;
+#endif
+	}
 }
 
-public class Clicker
-{
-    public bool clicked()
-    {
-#if (UNITY_ANDROID || UNITY_IPHONE)
-        return Cardboard.SDK.CardboardTriggered;
-#else
-return Input.anyKeyDown;
-#endif
-    }
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
 }
