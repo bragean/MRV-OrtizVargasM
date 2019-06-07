28a2b3d512d9b7cabc7464a82c9b6473f152d772
JesusOCH <44234311+JesusOCH@users.noreply.github.com>
2019-06-07 13:33:11 -0500

    Chapter 09 - Using All 360 Degrees

---
+++ VR_is_awesome/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Rotator : MonoBehaviour {
+	public float xRate = 0; // degrees per second
+	public float yRate = 20;
+	public float zRate = 0;
+	void Update () {
+		transform.Rotate (new Vector3 (xRate, yRate, zRate) *
+		                  Time.deltaTime);
+	}
+}
